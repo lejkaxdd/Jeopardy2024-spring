97ce4fa5617d91a27758ae27964a73f11b9ba5ae
lejkaxdd <lejkaxdd@yandex.ru>
2024-04-17 10:02:16 +0300

    change UI Task 1,2,4

--- Task 1/assets/js/main.js
+++ Task 1/assets/js/main.js
@@ -7,20 +7,21 @@ for (let i=0; i < btns.length; i++) {
 
         if (String(buttonBgColor) == "rgba(128, 37, 198, 0.8)") {
             let token = ("../assets/css/main.css")
-            document.getElementById("purpule").style.display = 'block';
-            document.getElementById("red").style.display = 'none';
-            document.getElementById("green").style.display = 'none';
+            
+            document.getElementById('trainpic').classList.remove('red');
+            document.getElementById('trainpic').classList.remove('green');
+            document.getElementById('trainpic').classList.add('purple');
 
         }
         else if (String(buttonBgColor) == "rgba(0, 128, 0, 0.8)"){
-            document.getElementById("green").style.display = 'block';
-            document.getElementById("red").style.display = 'none';
-            document.getElementById("purpule").style.display = 'none';
+            document.getElementById('trainpic').classList.remove('purple');
+            document.getElementById('trainpic').classList.remove('red');
+            document.getElementById('trainpic').classList.add('green');
         }
         else {
-            document.getElementById("purpule").style.display = 'none';
-            document.getElementById("red").style.display = 'block';
-            document.getElementById("green").style.display = 'none';
+            document.getElementById('trainpic').classList.remove('green');
+            document.getElementById('trainpic').classList.remove('purple');
+            document.getElementById('trainpic').classList.add('red');
         }
         
     });
